6e4ce4b255347211210893daf6a36137f3b35902
Meet <developer15.balajitech@gmail.com>
2024-07-27 14:24:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import { useEffect, useMemo, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import fragmentShader from "./fragment";
+import vertexShader from "./vertex";
+import { useControls } from "leva";
+
+function Scene() {
+  const mesh = useRef();
+  const speed = 0.3;
+  const colorA = "#3f3089";
+  const colorB = "#00bcff";
+  const intensity = 0.15;
+  const particalSize = 28.0;
+
+  const uniforms = useMemo(() => {
+    return {
+      u_time: {
+        value: 0.0
+      },
+      u_speed: {
+        value: speed
+      },
+      u_intensity: {
+        value: intensity
+      },
+      u_partical_size: {
+        value: particalSize
+      },
+      u_color_a: {
+        value: new THREE.Color(colorA)
+      },
+      u_color_b: {
+        value: new THREE.Color(colorB)
+      },
+      u_resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) } // Added resolution uniform
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      mesh.current.material.uniforms.u_resolution.value.set(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Call on mount
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useFrame((state) => {
+    const { clock } = state;
+    mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
+    mesh.current.material.uniforms.u_speed.value = speed;
+    mesh.current.material.uniforms.u_intensity.value = intensity;
+    mesh.current.material.uniforms.u_partical_size.value = particalSize;
+    mesh.current.material.uniforms.u_color_a.value = new THREE.Color(colorA);
+    mesh.current.material.uniforms.u_color_b.value = new THREE.Color(colorB);
+  });
+
+  return (
+    <>
+      <color args={["#000000"]} attach="background" />
+      <OrbitControls enableZoom={false} enablePan={false}  />
+      <ambientLight />
+      <points scale={1.5} ref={mesh}>
+        <icosahedronGeometry args={[2, 20]} />
+        <shaderMaterial
+          uniforms={uniforms}
+          fragmentShader={fragmentShader}
+          vertexShader={vertexShader}
+          blending={THREE.AdditiveBlending}
+          depthWrite={false}
+        />
+      </points>
+    </>
+  );
+}
+
+export default function Example() {
+  return (
+    <Canvas camera={{ position: [8, 0, 0] }}>
+      <Scene />
+    </Canvas>
+  );
+}
